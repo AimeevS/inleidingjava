2f2e8a4a5c33f380265af059750d07797e3598e2
Aimee <Aimeevanslingerlandt@hotmail.nl>
2021-01-22 13:34:57 +0100

    Initial commit

---
+++ src/H05/H051.java
@@ -0,0 +1,13 @@
+package H05;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H051 extends Applet {
+    public void init(){
+
+    }
+    public void paint(Graphics g){
+        g.drawString("Hello World!", 20, 20);
+    }
+}
---
+++ src/H4/H4.java
@@ -0,0 +1,13 @@
+package H4;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H4 extends Applet {
+    public void init(){
+
+    }
+    public void paint(Graphics g){
+        g.drawString("Hallo Wereld", 20, 20);
+    }
+}
